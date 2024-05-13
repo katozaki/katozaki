0a957e221643c9918537eb183ead60a2ae5e4253
katozaki <156406976+katozaki@users.noreply.github.com>
2024-05-13 22:00:51 +0800

    Add files via upload

---
+++ BankLogInApplication/src/bankloginapplication/BankLogInApplication.java
@@ -0,0 +1,16 @@
+
+package bankloginapplication;
+
+import Login.LoginApplication;
+
+
+public class BankLogInApplication {
+
+    public static void main(String[] args) {
+    LoginApplication LoginFrame = new LoginApplication();
+    LoginFrame.setVisible(true);
+    LoginFrame.pack();
+    LoginFrame.setLocationRelativeTo(null);
+    }
+    
+}
